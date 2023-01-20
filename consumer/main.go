b249e9ead12626ad0486d58cc3bb42191e74cba3
Lucas <lucascpinheiro10@gmail.com>
2023-01-19 22:08:45 -0300

    first commit

---
+++ consumer/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/segmentio/kafka-go"
+)
+
+func main() {
+	conn, err := kafka.DialLeader(context.Background(), "tcp", "localhost:9092", "topic_test", 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	conn.SetReadDeadline(time.Now().Add(time.Second * 10))
+
+	batch := conn.ReadBatch(10e3, 1e6)
+
+	b := make([]byte, 10e3)
+	for {
+		n, err := batch.Read(b)
+		if err != nil {
+			break
+		}
+		fmt.Println(string(b[:n]))
+	}
+
+	if err := batch.Close(); err != nil {
+		log.Fatal("failed to close batch:", err)
+	}
+
+	if err := conn.Close(); err != nil {
+		log.Fatal("failed to close connection:", err)
+	}
+}
---
+++ producer/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/segmentio/kafka-go"
+)
+
+func main() {
+	conn, err := kafka.DialLeader(context.Background(), "tcp", "localhost:9092", "topic_test", 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	conn.SetWriteDeadline(time.Now().Add(time.Second * 10))
+
+	conn.WriteMessages(kafka.Message{Value: []byte("hello kafka")})
+}
